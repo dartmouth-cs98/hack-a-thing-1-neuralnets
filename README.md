35e292752629cccbb2173e5924e293c7722cdc1e
Angela Li <alidartmouth20@gmail.com>
2019-09-21 13:50:28 -0400

    Added README and single perceptron neural network

---
+++ README.md
@@ -0,0 +1,11 @@
+# Neural Networks
+
+### Sia Peng and Angi Li
+
+## What we built
+
+## Who did What
+
+## What we learned
+
+## What didn't work
---
+++ fashion_categories.py
@@ -0,0 +1,24 @@
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from tensorflow.keras.utils import to_categorical
+import numpy as np
+
+# Single Perceptron Model
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+x_train = x_train.reshape(x_train.shape[0], -1) / 255.0
+x_test = x_test.reshape(x_test.shape[0], -1) / 255.0
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+# Single Perceptron Model
+model = Sequential()
+model.add(Dense(50, input_dim=784, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=10, validation_split=0.1)
+
+_, test_acc = model.evaluate(x_test, y_test)
+print(test_acc)
